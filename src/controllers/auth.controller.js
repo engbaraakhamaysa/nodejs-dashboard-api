b1f245c187d4da85ef570d47658045833fb92556
Baraa <engbaraakhamaysa@gmail.com>
2025-09-22 12:43:27 +0300

    Add generateTokens in auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,21 @@
 const { model } = require("mongoose");
 const User = require("../models/user.model");
+const {
+  JWT_SECRET,
+  JWT_REFRESH_SECRET,
+  JWT_EXPIRES,
+  JWT_REFRESH_EXPIRES,
+} = require("../config/token");
 
+const generateTokens = (userId) => {
+  const accessToken = jwt.sign({ id: userId }, JWT_SECRET, {
+    expiresIn: JWT_EXPIRES,
+  });
+  const refreshToken = jwt.sign({ id: userId }, JWT_REFRESH_SECRET, {
+    expiresIn: JWT_REFRESH_EXPIRES,
+  });
+  return { accessToken, refreshToken };
+};
 //Signup Controller
 const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -16,10 +31,13 @@ const signup = async (req, res) => {
 
     const newUser = new User({ name, email, password });
     await newUser.save();
+    const { accessToken, refreshToken } = generateTokens(newUser._id);
     res.status(200).json({
       _id: newUser._id,
       name: newUser.name,
       email: newUser.email,
+      accessToken,
+      refreshToken,
     });
   } catch (error) {
     if (error.code === 11000) {
@@ -37,8 +55,14 @@ const login = async (req, res) => {
     if (!user || password !== user.password) {
       return res.status(400).json({ message: "Incorrect email or password" });
     }
+    const { accessToken, refreshToken } = generateTokens(user._id);
 
-    res.json({ message: "Login Successful", email: user.email });
+    res.json({
+      message: "Login Successful",
+      email: user.email,
+      accessToken,
+      refreshToken,
+    });
   } catch (error) {
     res.status(500).json({ message: "Sever error", error: error.message });
   }
