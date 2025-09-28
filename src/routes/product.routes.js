1ac485b21ffe9678c4950aa64796c7b4b5b136b4
Baraa <engbaraakhamaysa@gmail.com>
2025-09-28 23:37:24 +0300

    Add Routes Update Product

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -6,6 +6,7 @@ const {
   createProduct,
   getAllProducts,
   deleteProduct,
+  updateProduct,
 } = require("../controllers/product.controller");
 
 //Multer Save Images
@@ -23,5 +24,6 @@ const upload = multer({ storage });
 router.post("/create", upload.single("image"), createProduct);
 router.get("/allproducts", getAllProducts);
 router.delete("/deleteproduct/:id", deleteProduct);
+router.post("/update/:id", upload.single("image"), updateProduct);
 
 module.exports = router;
